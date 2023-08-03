967915a4b70e7eda1129966a0065e05f861f214b
eleftherioseleftheriadis <p17elev@ionio.gr>
2023-08-03 10:43:02 +0000

    d

---
+++ code/models.sql
@@ -0,0 +1,26 @@
+GRANT ALL PRIVILEGES ON name.* TO ‘root’@‘localhost' IDENTIFIED BY ‘root’;
+FLUSH PRIVILEGES;
+CREATE DATABASE name;
+SET PASSWORD FOR ‘root’@‘localhost’ = PASSWORD(‘root’);
+CREATE DATABASE wacky_db;
+CREATE TABLE user_roles (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+  PRIMARY KEY (user_id, role_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
+CREATE TABLE roles (
+  role_id INT AUTO_INCREMENT PRIMARY KEY,
+  role_name VARCHAR(50) NOT NULL
+);
+CREATE TABLE users (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  wacky_question VARCHAR(255) NOT NULL
+);
+
